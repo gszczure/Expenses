c11cd57423cc8b8fa62b2e690358ed902a0b18ea
gszczure <grzegorz.sz1202@gmail.com>
2026-06-04 17:53:06 +0200

    refactor: remove unused import

--- src/main/java/org/example/service/SummaryService.java
+++ src/main/java/org/example/service/SummaryService.java
@@ -6,7 +6,6 @@ import org.example.model.AccountTransaction;
 import org.example.model.TransactionType;
 import org.example.repository.AccountTransactionRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
